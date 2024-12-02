14577a2cf95e56aa2cbe49942c9d1230d48c209d
Sidar Yilmaz <sidaryilmazengineering@outlook.com>
2024-12-02 16:08:33 +0300

    second commit

---
+++ team.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const teamSchema = new Schema(
+  {
+    id:{
+      type:String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,  
+      unique: true,    
+    },
+    description: {
+      type: String,
+      maxlength: 200,  
+      default: 'No description provided', 
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',                         
+        required: true,
+      },
+    ],
+    
+  },
+  { timestamps: true }  
+);
+const Team = mongoose.model('Team', teamSchema);
+
+module.exports = Team;
