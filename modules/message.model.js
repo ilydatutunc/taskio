50e1e9bc43fd1dabded3b6a66471496f33b243fd
Sidar Yilmaz <sidaryilmazengineering@outlook.com>
2024-12-08 15:18:21 +0300

    schemalar eklendi.

---
+++ modules/message.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const messageSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+  },
+    text: [
+    { 
+      type: String,
+      required: true, 
+    },
+  ]
+    sender: {
+      type : mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now, 
+    },
+  
+    attachment: {
+      type: String, 
+      default: null,
+    },
+  },
+  { timestamps: true,  collection: "messages" }
+);
+
+
+const Message = mongoose.model("Message"messageSchema);
+
+module.exports = Message;
+
